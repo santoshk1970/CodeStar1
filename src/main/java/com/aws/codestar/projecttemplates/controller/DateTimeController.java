6654b753faca5b24e6cc49f6dd6a02d7be962d59
Santosh <kulkarnisg@gmail.com>
2021-01-13 21:08:36 +0000

    New controller

---
+++ src/main/java/com/aws/codestar/projecttemplates/controller/DateTimeController.java
@@ -0,0 +1,37 @@
+package com.aws.codestar.projecttemplates.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+
+/**
+ * Basic Spring MVC controller that handles all GET requests.
+ */
+@Controller
+@RequestMapping("/getDateTime")
+public class DateTimeController {
+
+    private final String siteName;
+
+    public DateTimeController(final String siteName) {
+        this.siteName = siteName;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView datTime() {
+       var now = LocalDateTime.now();
+        var dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        var date_time = dtf.format(now);
+
+        var params = new HashMap<String, Object>();
+        params.put("date_time", date_time);
+
+        return new ModelAndView("showMessage", params);
+    }
+   
+
+}
