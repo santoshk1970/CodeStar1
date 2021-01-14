9e92266e87a682fabad521c60627b143616347b8
Santosh <kulkarnisg@gmail.com>
2021-01-14 20:07:40 +0000

    fixed typo

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 /**
  * Basic Spring MVC controller that handles all GET requests.
