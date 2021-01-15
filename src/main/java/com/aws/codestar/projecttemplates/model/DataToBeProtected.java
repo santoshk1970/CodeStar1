301b9cb61d5e6e36c7ebf1d9c4089175cee1ab17
Santosh Kulkarni <kulkarnisg@gmail.com>
2021-01-15 10:22:30 -0600

    adding model

---
+++ src/main/java/com/aws/codestar/projecttemplates/model/DataToBeProtected.java
@@ -0,0 +1,25 @@
+package com.aws.codestar.projecttemplates.model;
+
+public class DataToBeProtected {
+private String clearValue;
+private String dataElementName;
+private String userId;
+public String getClearValue() {
+	return clearValue;
+}
+public void setClearValue(String clearValue) {
+	this.clearValue = clearValue;
+}
+public String getDataElementName() {
+	return dataElementName;
+}
+public void setDataElementName(String dataElementName) {
+	this.dataElementName = dataElementName;
+}
+public String getUserId() {
+	return userId;
+}
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+}
