78e04b8c024854bccc3e76eaacbff728d407e83e
Santosh Kulkarni <kulkarnisg@gmail.com>
2021-01-17 23:50:47 -0600

    parsing input

--- src/main/java/com/aws/codestar/projecttemplates/service/impl/DataProtectorImpl.java
+++ src/main/java/com/aws/codestar/projecttemplates/service/impl/DataProtectorImpl.java
@@ -1,10 +1,17 @@
 package com.aws.codestar.projecttemplates.service.impl;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.aws.codestar.projecttemplates.service.DataProtector;
 import com.aws.codestar.projecttemplates.service.aws.LambdaInvoker;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class DataProtectorImpl implements DataProtector {
@@ -13,7 +20,24 @@ public class DataProtectorImpl implements DataProtector {
 	@Override
 	public String transform(String clearValue, String dataElementName, String userId) {
 		String returnValue=lambdaInvoker.invokeTransformLambda();
-		return clearValue.concat("transformed"+returnValue);//comment
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String,String> myMap = new HashMap<String, String>();
+		byte[] mapData = returnValue.getBytes();
+		try {
+			myMap = objectMapper.readValue(mapData, HashMap.class);
+			returnValue=myMap.get("message");
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return clearValue.concat("-X-"+returnValue);//comment
 	}
 
 }
